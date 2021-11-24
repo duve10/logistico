d8e7eb506c99df1aa8bbb2d4d2481b7e48cf9356
anderson <andersonromeroloarte@gmail.com>
2021-11-23 23:55:29 -0500

    migrations

---
+++ database/migrations/2021_11_24_035803_create_perfil_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfil', function (Blueprint $table) {
+            $table->integer('nro_doc')->primary();
+            $table->string('nombres',50);
+            $table->string('ap_paterno',30);
+            $table->string('ap_materno',30);
+            $table->string('direccion',200)->nullable();
+            $table->string('celular',9);
+            $table->string('correo',100)->unique();
+            $table->date('fecha_nacimiento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfil');
+    }
+}
